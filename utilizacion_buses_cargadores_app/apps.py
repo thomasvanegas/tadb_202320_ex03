28b08898f6ff570b5633ee275e029ab6f9a21d6f
thomasvanegas <vanegasacevedothomas@gmail.com>
2023-10-10 09:45:44 -0500

    Inicializacion del proyecto usando el framework Django

---
+++ utilizacion_buses_cargadores_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UtilizacionBusesCargadoresAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'utilizacion_buses_cargadores_app'
