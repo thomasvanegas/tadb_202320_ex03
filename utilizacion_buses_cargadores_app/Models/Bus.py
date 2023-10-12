ea16e2f78bf62694a22f1205a1938f843a92d515
thomasvanegas <vanegasacevedothomas@gmail.com>
2023-10-12 16:23:58 -0500

    Se elimina el archivo models.py y se configura correctamente el directorio de Models

--- utilizacion_buses_cargadores_app/Models/Bus.py
+++ utilizacion_buses_cargadores_app/Models/Bus.py
@@ -12,7 +12,7 @@ def validar_placa(value):
 # Cada modelo (tabla) es una clase de la subclase models.Model
 class Bus(models.Model):
     id = models.AutoField("Identificador Único de Cada Bus", primary_key=True, editable=False)
-    placa = models.CharField("Placa del Bus", validators=[validar_placa], max_length=6, db_index=True)
+    placa = models.CharField("Placa del Bus", max_length=6, db_index=True)
     estado_id = models.ForeignKey(EstadoBus.EstadoBus, on_delete=models.CASCADE, to_field='id')
 
     class Meta:
--- utilizacion_buses_cargadores_app/models.py
+++
@@ -1,3 +0,0 @@
-# Se importantan todos los modelos
-from utilizacion_buses_cargadores_app.Models.Bus import Bus
-from utilizacion_buses_cargadores_app.Models.EstadoBus import EstadoBus
