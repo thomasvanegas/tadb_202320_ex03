d13907f0d8d321c43d2309cb91bdc39b8153bdad
ElianaGiraldo-98 <eliana.giraldod@upb.edu.co>
2023-10-13 15:55:40 -0500

    Creación de BusService.py

--- utilizacion_buses_cargadores_app/Service/BusService.py
+++ utilizacion_buses_cargadores_app/Service/BusService.py
@@ -1,52 +1,41 @@
-from utilizacion_buses_cargadores_app.Helpers import AppValidationException, DbOperationException
-from utilizacion_buses_cargadores_app.Interfaces import BusRepository
-from utilizacion_buses_cargadores_app.Models import Bus
+from .models import Bus
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 
 class BusService:
-    def __init__(self, bus_repository:  BusRepository):
-        self._bus_repository= bus_repository
-
-    async def get_all_async(self):
-        return await self._bus_repository.get_all_async()
     
-    async def get_details_by_id_async(self, bus_id):
-        unbus = await self._bus_repository.get_details_by_id_async(bus_id)
-        if unbus.id == 0:
-            raise AppValidationException(f"No se encontro el bus con el id {bus_id}")
-        return unbus
- 
-    async def create_async(self, bus):
-        if not bus.placa:
-            raise AppValidationException("No se puede insertar una placa nula")
-        if not bus.estado_id:
-            raise AppValidationException("No se puede insertar un estado nulo")
-        
-        bus_existente = await self._bus_repository.get_by_placa_async(bus.placa)
-        
-        if bus_existente.id != 0:
-            raise AppValidationException(f"Existe un bus con la placa {bus.placa}")
+    def create_bus(id, placa, estado_id):
         try:
-             bus_resultado= await self._bus_repository.create_async(bus)
-        
-            if not bus_resultado:
-                raise AppValidationException("No se agregaron cambios")
-            bus_existente = await self._bus_repository.get_by_placa_async(bus.placa)
-        except DbOperationException as error:
-            raise error
-        return bus_existente
-    
-    async def delete_async(self, bus_id):
-        
-        bus_existente = await self._bus_repository.get_by_id_async(bus_id)
-
-        if bus_existente.id == 0:
-            raise AppValidationException(f"No existe un bus con el id{bus_id} que se pueda eliminar")
-
+            if Bus.objects.filter(id=id).exists():
+                raise ValidationError("Ya existe un Bus con este id.")
+            bus = Bus(id=id, placa=placa, estado_id=estado_id)
+            bus.full_clean()
+            bus.save()
+            return bus
+        except ValidationError as e:
+            raise e  
+            
+    def get_bus_by_id(bus_id):
         try:
-           bus_resultado = await self._bus_repository.delete_async(bus_existente)
+            return Bus.objects.get(id=bus_id)
+        except ObjectDoesNotExist:
+            raise Exception("El bus no existe.")
+            
+    def update_bus(bus_id, id, placa, estado_id):
+        try:
+            bus = BusService.get_bus_by_id(bus_id)
+            
+            if Bus.objects.filter(id=id).exclude(id=bus_id).exists():
+                raise ValidationError("Ya existe un Bus con este id.")
 
-            if not bus_resultado:
-                raise AppValidationException(" No generó cambios en la DB")
-        except DbOperationException as error:
-            raise error
+            bus.id = id
+            bus.placa = placa
+            bus.estado_id = estado_id
+            bus.full_clean() 
+            bus.save()
+            return bus
+            
+        except ValidationError as e:
+            raise e  # Maneja la validación de reglas de negocio
+        except ObjectDoesNotExist:
+            raise Exception("El bus no existe.")
     
